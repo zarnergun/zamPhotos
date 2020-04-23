8fd3e59365696860611cfa280d0de15d2a77fda0
zarnergun <deceirem@gmail.com>
2020-04-23 14:53:29 +0200

    database

--- app/src/main/java/com/zam/photos/MainActivity.kt
+++ app/src/main/java/com/zam/photos/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.zam.photos
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
@@ -9,11 +10,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.iid.FirebaseInstanceId
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Target
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     private var name = ""
 
     companion object {
+        private val TAG = "MainActivity"
         fun getLaunchIntent(from: Context) = Intent(from, MainActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         }
@@ -50,6 +56,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -70,11 +77,35 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false);
 
+        logTokenButton.setOnClickListener {
+            FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w(TAG, "getInstanceId failed", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+
+                    // Log and toast
+                    val msg = getString(R.string.msg_token_fmt, token)
+                    Log.i("BLABLA", token)
+                    Log.d(TAG, msg)
+                    Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+                })
+
+        }
+
     }
 
     private fun signOut() {
         startActivity(LoginFirebaseActivity.getLaunchIntent(this))
         FirebaseAuth.getInstance().signOut();
+    }
+
+    public fun ToastMessage() {
+
     }
 
 
